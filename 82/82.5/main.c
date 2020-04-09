4b30d92bcb33806e997fd3770b9f7df75581d2dc
jdo <jdo@student.42seoul.kr>
2020-04-09 22:14:41 +0900

    add list

--- 82/82.5/main.c
+++ 82/82.5/main.c
@@ -20,10 +20,16 @@ typedef struct _FILE_DESC {
 
 #pragma pack(pop)
 
+typedef struct _FILE_NODE {
+	struct _FILE_NODE *next;
+	FILE_DESC desc;
+} FILE_NODE, *PFILE_NODE;
+
 //프로그램에서만 사용하는 구조체
 typedef struct _ARCHIVE {
 	ARCHIVE_HEADER header;
 	FILE *fp;
+	FILE_NODE fileList;
 } ARCHIVE, *PARCHIVE;
 
 #define ARCHIVE_NAME "archive.bin"
@@ -40,6 +46,18 @@ uint32_t getFileSize(FILE *fp)
 	fseek(fp, currPos, SEEK_SET);
 
 	return size;
+}
+
+void list(PARCHIVE archive)
+{
+	printf("파일 목록 : \n");
+
+	PFILE_NODE curr = archive->fileList.next;
+	while (curr != NULL)
+	{
+		printf("    %s\n", curr->desc.name);
+		curr = curr->next;
+	}
 }
 
 //파일 추가 함수 정의
@@ -110,6 +128,8 @@ int main()
 	PARCHIVE archive = malloc(sizeof(ARCHIVE));
 	memset(archive, 0, sizeof(ARCHIVE));
 
+	PFILE_NODE curr;
+
 	FILE *fp = fopen(ARCHIVE_NAME, "r+b");
 	if (fp == NULL)
 	{
@@ -119,17 +139,77 @@ int main()
 		//새 아카이브 헤더 생성
 		archive->header.magic = 'AF'; //매직 넘버 AF저장
 		archive->header.version = 1;
+
 		//아카이브 파일에 아카이브 헤더 저장
 		if (fwrite(&archive->header, sizeof(ARCHIVE_HEADER), 1, fp) < 1)
 		{
-			printf("아카이브 헤더 쓰기 실패 \n");
+			printf("아카이브 헤더 쓰기 실패\n");
 			fclose(fp);
 			return -1;
 		}
-		//아카이브 파일 포인터 저장
-		archive->fp = fp;
-		append(archive, "hello.txt");
 	}
-	fclose(fp);
+	else
+	{
+		if (fread(&archive->header, sizeof(ARCHIVE_HEADER), 1, fp) < 1)
+		{
+			printf("아카이브 헤더 읽기 실패 \n");
+			fclose(fp);
+			return -1;
+		}
+	}
+
+	//아카이브 파일 매직 넘버 검사
+	if (archive->header.magic != 'AF')
+	{
+		printf("아카이브 파일이 아닙니다.\n");
+		fclose(fp);
+		return -1;
+	}
+
+	//아카이브 파일 버전 검사
+	if (archive->header.version != 1)
+	{
+		printf("버전이 맞지 않습니다.\n");
+		fclose(fp);
+		return -1;
+	}
+	archive->fp = fp;
+
+	int ret = 0;
+	uint32_t size = getFileSize(fp);
+	uint32_t currPos = ftell(fp);
+
+	while (size > currPos)
+	{
+		PFILE_NODE node = malloc(sizeof(FILE_NODE));
+		memset(node, 0, sizeof(FILE_NODE));
+
+		if (fread(&node->desc, sizeof(FILE_DESC), 1, fp) < 1)
+		{
+			printf("아카이브 파일 읽기 실패\n");
+			free(node);
+			ret = -1;
+			goto FINALIZE;
+		}
+
+		// 연결 리스트에 파일 정보 노드(FILE_NODE) 추가
+		node->next = archive->fileList.next;
+		archive->fileList.next = node;
+
+		// 현재 파일 포인터의 위치에 파일의 크기를 더하여 다음 파일 정보 위치로 이동
+		currPos = ftell(fp) + node->desc.size;
+		fseek(fp, currPos, SEEK_SET);
+	}
+	list(archive);
+
+FINALIZE:
+	curr = archive->fileList.next;
+	while (curr != NULL)
+	{
+		PFILE_NODE next = curr->next;
+		free(curr);
+		curr = next;
+	}
+	fclose(archive->fp);
 	free(archive);
 }
